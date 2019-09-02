84dd9c3141e60ae2c61743318932d1143c60e9cd
empier420 <matthewadams024@gmail.com>
2019-09-01 21:58:24 -0400

    Create scape.js

---
+++ scape.js
@@ -0,0 +1,63 @@
+Over 4,000 years ago:
+1st and 2nd age= the world was being made by Saradonian, Zaros and Guthix. (The god wars start in the 2nd age). 
+Creatures by gods:
+Zaros(god of pursue and passion): dark wizards, dragons, demons, vampires, werewolves, bats, chaos element, zombies, necromancy, etc.
+Saradominists(god of knowledge and wisdom): wizards, dwarfs, gnomes, goblins, fairies, elves, trolls, giants, ogres, etc.
+Guthix(god of creativity and power): humans, all insect monsters, all animal monsters, minerals, runes, nature, etc.   
+
+End of 2nd age= Zamorack a moempier4ertal kills Zaros with the staff of Armadyl
+Between 3,000-4,000 years ago
+
+3rd age = the god wars is over. People set up temples to the gods who fought. The 3 main surviving gods are Zamarack, Saradonium and Guthix.  
+Around 2,000 years ago 
+  
+4th age= civilization is destroyed by fighting over gods and people are nomadic. Humans fight with monsters over land. Humans try to re-establish land and gods again.
+
+Late 4th age= humans begin to spread out over the map.
+Around a few hundred years ago
+
+Early 5th age= humans find Guthix’s rune essence and begin to create the human kingdoms.
+
+Late 5th age= close to time of present humans are driving out the Zaros and Saradomists monsters by uniting and also fighting each other for power over the runes.  
+
+Late 5th age= fighting over rune essence power still remains.
+
+
+
+
+
+
+Metals: Weaponology (Value increases going down)
+Bronze: consists of about 88% copper and 12% tin. A ductile metal that was originally harder than iron while iron was being badly refined. Does not oxidize. 
+Strength: ++
+
+Iron: Oxidizes, giving a black tarnished outer surface when refined. Refining an iron deposit and separating it from the common minerals around it like nickel. Iron refining requires a blast furnace unlike bronze. 
+Strength: ++++
+
+Silver: lower oxidation properties than bronze. Very low caustic (does not get absorbed by plasma in the blood). Lower strength than bronze but can be molded and sharpened to be sharper than iron. 
+Strength: +
+
+Steel: Very high strength compared to pure Iron. Steel is typically 2% carbon and 98% iron. Steel takes a long time to produce and needs a blast furnace. 
+Strength: ++++++++
+
+Gold: A precious metal found in the crust. The most malleable and moldable metal on Earth.  The most noble of the noble metals (oxidation resistant). Mined from Electrum (an ore containing 20% silver and 80% gold).
+Alchemy and Chemistry studied gold in medieval times to do its high preservation values, which also lead to it being used as a universal currency.
+Strength: +++++
+
+Mithril: A magical form of silver with a bluish tint to it, possibly meaning it has lower oxidation values than silver. It is also stronger and lighter than steel; equivalent to modern day titanium alloys. 
+Strength: ++++++++++++
+
+Adamantium: Very unique and rare stone. A type of diamond that can be molded into a form. In addition to its hardness it has magnetic properties and can absorb almost any physical energy, making the metal itself when refined indestructible. 
+Strength: ++++++++++++++++++++
+
+Rune: A stone with an inscription on it from the 1st age. The stone and the inscription are from the gods. The stone itself is completely impregnable because it’s not found on Earth and has other worldly magical properties and strength.   
+Strength: ++++++++++++++++++++++++++++++++++++++++
+
+Dragon Stone/Gems/elf crystals:  Not found naturally on Earth or deposited from the 1st age. It’s a mineral that was left behind after the god wars by the gods themselves. No tool can mold it, 
+however dragon tips can be made from a gem with superior crafting skills as well as other very rare gems left by the gods.
+Dragon Armor and Weapons cannot be made so the few that exist from the god wars given to mortals to fight are extremely expensive in the 5th age. Elf crystals are gems given to the elven race by a god working for saradonian.   
+Strength: ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++  
+ 
+God weapons: Either priceless staffs that cannot be sold or bought. Or Weapons and armor used only by the god’s themselves and their celestial armies. Not only can they not be made by mortals. Their rarity is legendary and their value astronomical.
+Only the most powerful can even wield such weapons, in which they themselves are designed to destroy the very creations made by the gods. 
+Strength: ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++   
