c89b856cccf1f6bfe4c540910501943e8f302887
empier420 <matthewadams024@gmail.com>
2019-11-30 17:01:48 -0500

    Update scape.js

--- scape.js
+++ scape.js
@@ -5,7 +5,7 @@ Zaros(god of pursue and passion): dark wizards, dragons, demons, vampires, werew
 Saradominists(god of knowledge and wisdom): wizards, dwarfs, gnomes, goblins, fairies, elves, trolls, giants, ogres, etc.
 Guthix(god of creativity and power): humans, all insect monsters, all animal monsters, minerals, runes, nature, etc.   
 
-End of 2nd age= Zamorack a moempier4ertal kills Zaros with the staff of Armadyl
+End of 2nd age= Zamorack a mortal kills Zaros with the staff of Armadyl
 Between 3,000-4,000 years ago
 
 3rd age = the god wars is over. People set up temples to the gods who fought. The 3 main surviving gods are Zamarack, Saradonium and Guthix.  
@@ -22,6 +22,50 @@ Late 5th age= close to time of present humans are driving out the Zaros and Sara
 
 Late 5th age= fighting over rune essence power still remains.
 
+*
+Ruling Class: Guthix: humans; Kingdom of Asgarnia, 
+(City of Falador, Town of Rimmington, Port of Sarim, Mountain city of Burthorpe, warriors guild, crafting guild, mining guild and prayer guild).
+ manufacturing, crafting, law and commerce. Ally to Saradominist Dwarven mine and troll stronghold, help with production and war. 
+
+*
+ Ruling Class: Guthix: humans; kingdom of Misthalin, 
+ (City of Varrock, Town of Lumbridge, Draynor Village, Dessert city of Alkarid,  Wizard’s tower, Mage arena, cooking guild and champions guild). 
+Power, passion, skill, will and exploration. Ally to Zaros’s hold of rune essence and abyss, help with knowledge of magic, and holding wealth and power.
+
+*
+Ruling Class: Guthix: humans; kingdom of Kandarin, 
+(Cidetal of Ardougne, Port of Khazard, magic city of Yanille, Port City of Catherby, Seers Village, Camelot castle, fremennik province, barbarian land, ranging guild, magic guild and legend’s guild).
+ Knowledge, learning, freedom, diversity and community. Ally to Saradominist Tree Gnome village and stronghold, trading knowledge and practices, uses a diverse knowledge of magic and technical services from the Gnomes.
+
+ *
+ Ruling Class: Guthix: humans; kingdom of Great Kourend, 
+(Kourend Castle). 
+Creativity, order, reason and logic. Ally to Guthix Humans hold the castle and use their own reason and logic by Guthix to aide in commerce, war and production. Is also a meeting point for peace talks for the surrounding kingdoms. ).  
+(City of Shayzein and city of Arceuus).
+Power, war, dominance, self-preservation and magic. Ally to Zaros In Arceuus people use self- preservation and magic in order to attempt to be immortal. From Zammarok defeating Zaros in order to become a god. Their power is drawn from Zaros’s dark alter. In Shayzein people use power, war and dominance in order to push their enemies away and tank a dominant stand on where they are.
+(City of Hosidius, Port Pisccarilius and City of Lovakengi).
+Knowledge, production, caring, community and commerce. Ally to Saradominist In Hosidius people use production and carring to create a thricing farming community. In Pisccarilius people use commerce and carring to produce a thriving fishing community. In Lovakengi people use knowledge and production to use the minerals and resorses from a volcano to create a thriving manufacturing community.     
+
+_
+Ruling Class: Zaros: Vampire and Werewolf; Morytania
+(Citedel and Keep of Darkmeyer, Slepe, Burgh de Rott, Mort’ton, Canifs and Port Phasmatys).
+Power, pain, overpowering, wealth and death. Ally to Zaros The whole territory is in control of either Werewolf protection or Vampire tithe. Humans are all enslaved to these creatures in Moryania. In addition Port Phasmatys tried to follow Zaros by trying to be immortal but instead became trapped by the port by feral vampires. The town of Mort’ton was struck by a zombie plague. Burgh de Rott is in poverty from the overpowering citadel of Darkmeyer, which is run by vampiers who take blood tithes from the people. The citadel of Darkmeyer used to be a powerful city like Varrok. Canifs is run by werewolves who inspect humans who enter Moryania.    
+
+#
+Ruling Class: Saradomin: Elves; Tirannwn
+(Citadel of Prifddinas, town of Gwenith, town of Lletya, Iorwerth camp and Tyras camp).
+Knowledge, mystery, intricacy, production and utilization. Ally to Saradomin The area is controlled by a very powerful citadel called Prifddinas magically preserved and cloaked from the 2nd age. The underground pass was designed by the elves to keep humans out of Tirannwn. The underground pass is a destroyed Zaros foothold.   
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -29,68 +73,73 @@ Late 5th age= fighting over rune essence power still remains.
 
 Metals: Weaponology (Value increases going down)
 Bronze: consists of about 88% copper and 12% tin. A ductile metal that was originally harder than iron while iron was being badly refined. Does not oxidize. 
-Strength: ++
+Strength: +++++ 5
 
 Iron: Oxidizes, giving a black tarnished outer surface when refined. Refining an iron deposit and separating it from the common minerals around it like nickel. Iron refining requires a blast furnace unlike bronze. 
-Strength: ++++
+Strength: +++++++ 7
 
 Silver: lower oxidation properties than bronze. Very low caustic (does not get absorbed by plasma in the blood). Lower strength than bronze but can be molded and sharpened to be sharper than iron. 
-Strength: +
+Strength: +1
 
 Steel: Very high strength compared to pure Iron. Steel is typically 2% carbon and 98% iron. Steel takes a long time to produce and needs a blast furnace. 
-Strength: ++++++++
+Strength: ++++++++++ 10
 
 Gold: A precious metal found in the crust. The most malleable and moldable metal on Earth.  The most noble of the noble metals (oxidation resistant). Mined from Electrum (an ore containing 20% silver and 80% gold).
 Alchemy and Chemistry studied gold in medieval times to do its high preservation values, which also lead to it being used as a universal currency.
-Strength: +++++
+Strength: +++++ 5
 
 Mithril: A magical form of silver with a bluish tint to it, possibly meaning it has lower oxidation values than silver. It is also stronger and lighter than steel; equivalent to modern day titanium alloys. 
-Strength: ++++++++++++
+Strength: +++++++++++++++ 15
 
 Adamantium: Very unique and rare stone. A type of diamond that can be molded into a form. In addition to its hardness it has magnetic properties and can absorb almost any physical energy, making the metal itself when refined indestructible. 
-Strength: ++++++++++++++++++++
+Strength: ++++++++++++++++++++++ 22
 
 Rune: A stone with an inscription on it from the 1st age. The stone and the inscription are from the gods. The stone itself is completely impregnable because it’s not found on Earth and has other worldly magical properties and strength.   
-Strength: ++++++++++++++++++++++++++++++++++++++++
+Strength: ++++++++++++++++++++++++++++ 28
 
 Dragon Stone/Gems/elf crystals:  Not found naturally on Earth or deposited from the 1st age. It’s a mineral that was left behind after the god wars by the gods themselves. No tool can mold it, 
 however dragon tips can be made from a gem with superior crafting skills as well as other very rare gems left by the gods.
 Dragon Armor and Weapons cannot be made so the few that exist from the god wars given to mortals to fight are extremely expensive in the 5th age. Elf crystals are gems given to the elven race by a god working for saradonian.   
-Strength: ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++  
+Strength: :++++++++++++++++++++++++++++++++++++ 37   
  
 God weapons: Either priceless staffs that cannot be sold or bought. Or Weapons and armor used only by the god’s themselves and their celestial armies. Not only can they not be made by mortals. Their rarity is legendary and their value astronomical.
 Only the most powerful can even wield such weapons, in which they themselves are designed to destroy the very creations made by the gods. 
-Strength: ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ 
+Strength: ++++++++++++++++++++++++++++++++++++++++++ 41 
 
 
 
 Magical Material: Magic ology increasing value going down
 Wizard: material from the second age. Worn by the common mortal, and valued high in the 5th age.
 Mana:
-+++++
++ 1
 Mystic: A series of elaborate robes worn by the leaders of the second age. The have prominent and dominating outfits, so that they could command their subordinates. 
 Mana:
-++++++++++++
++++++++++ 9
 Ancestral/ elder: A series of fancy and peaceful looking robes. These were worn by the people of the 1st age. They are not made during war like the others. 
 Mana:
-++++++++++++++++++++++++
+++++++++++++++ 14
 
 Wood Material: Tree Ology increasing going down
  Evergreen/common tree: Found commonly all over the map. Except for desserts. The tree has been around only in the 5th age, and is used from construction to weaponry. 
 Resilience: 
-+++++
+++ 2
+
 Oak/dense wooded areas: From the 4th age originally. This somewhat uncommon wood is found in dense wooded areas. The tree was spawned when humans stopped using trees as much after the god wars, because most were nomadic tribes. However going into the 5th age, this tree was used up by rising cities when they found its superior resilience to the common tree, which was sickly compared to the Oak and dense wooded areas. 
 Resilience:
-+++++++++++++++
-   Willow/ Teak/ Maple/ very dense: These trees are originally from the 3rd age. Spawned out of the devotion to the gods for elemental resources. Guthix (god of nature), spawned the Willow tree for a watery resource, Teak for a resource in humid lands and Maple for a very cold and dry resource.
-Resilience:
-++++++++++++++++++++++++++++++++++
+++++ 4
+  
+Willow/ Teak/ Maple/ very dense: These trees are originally from the 3rd age. Spawned out of the devotion to the gods for elemental resources. Guthix (god of nature), spawned the Willow tree for a watery resource, Teak for a resource in humid lands and Maple for a very cold and dry resource.
+Willow Resilience: +++++ 5
+Teak Resilience: +++++++ 7
+Maple Resilience: +++++++ 7
+
 Yew / Mahogany/ Arctic Pine: From the beginning of the 3rd age immediately after the god wars ended. Guthix made the three woods in order to strive peace between the creations. Yew for wooded areas, Mahogany for hot and humid areas and Arctic Pine for cold regions. However war soon started up again, so Guthix spawned the much lesser trees instead Willow, Teak and Maple. 
 Resilience:
-++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ 
+++++++++++++ 12  
+
 Magic/ Red Wood: Spawned in the 1st age by Guthix as a prized wood. Most of the trees were burned in the 2nd age by the god wars, and are only kept alive by magical preserved areas around the map. Much lesser trees were spawned by Guthix in the 3rd, 4th and 5th age.  
 Resilience:
-++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++  
++++++++++++++++++ 17  
 
 Crafting: Runes
 All from the essence, from the 1st age by Guthix. 
